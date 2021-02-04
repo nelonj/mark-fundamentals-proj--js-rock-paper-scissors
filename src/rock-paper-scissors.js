a79f9fce952c0beed7b15987ebf13b021ad19fea
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-04 17:22:59 +0000

    comments added for my understanding

--- src/rock-paper-scissors.js
+++ src/rock-paper-scissors.js
@@ -1,24 +1,29 @@
 // loading a function from an external dependency
 const prompt = require("readline-sync").question;
 
+
+// keys: [values] form an Object, which is equivalent to Python dictionary
 const acceptableVariants = {
   rock: ["rock", "Rock", "r", "R"],
   paper: ["paper", "Paper", "p", "P"],
   scissors: ["scissors", "Scissors", "s", "S"],
 };
 
+
+// checking if user has inputted an acceptable Variant + returning the standardised lowercase name of the Variant
 function asStandardChoice(inputStr) {
   /** An nested array: array of array of strings */
-  const arrayOfVariantArrays = Object.values(acceptableVariants);
-
+  const arrayOfVariantArrays = Object.values(acceptableVariants); // big array of 3 arrays of strings
+  console.log(arrayOfVariantArrays);
   // alternative `for ... of ...` loop syntax - great for arrays
-  for (let variantArray of arrayOfVariantArrays) {
+  for (let variantArray of arrayOfVariantArrays) { // chooses a smaller array, and iterates through a number of times equivalent to items of larger array
     if (variantArray.includes(inputStr)) {
       return variantArray[0];
     }
   }
 }
 
+// make a result message and print that
 function declareWinner(userPick, computerPick) {
   const resultMessage = makeResultMessage(userPick, computerPick);
   console.log(resultMessage);
@@ -27,16 +32,20 @@ function declareWinner(userPick, computerPick) {
 /**
  * Check if the first choice beats the second choice
  */
-function isWinningChoice(firstChoice, secondChoice) {
-  const weaknesses = {
+function isWinningChoice(userChoice, computerChoice) {
+  const weaknesses = {  //an Object where values beat the keys
     rock: "paper",
     paper: "scissors",
     scissors: "rock",
   };
-  return weaknesses[secondChoice] === firstChoice;
+  // selects value, which is what will win over the computer's choice
+  return weaknesses[computerChoice] === userChoice; //returns a boolean
 }
 
+
+// my choice (1st of 2 terminal outputs)
 function getUserChoice() {
+  // allows it to prompt again if I insert something that doesn't work + stops when i 'return' something that will terminate the function itself
   while (true) {
     const answer = prompt("Your choice: rock, paper or scissors? \n> ");
     const standardisedChoice = asStandardChoice(answer);
@@ -52,6 +61,7 @@ function getUserChoice() {
   }
 }
 
+// 2nd of 2 terminal outputs
 function makeResultMessage(userChoice, computerChoice) {
   if (userChoice === computerChoice) {
     return `You both chose ${computerChoice} - it's a draw!`;
@@ -62,18 +72,25 @@ function makeResultMessage(userChoice, computerChoice) {
   }
 }
 
+
+// computer's choice
+function randomPick() {
+  const choiceOptions = Object.keys(acceptableVariants);
+  // multiply key length (3) by a decimal between 0 and 1, and then rounds it down. Doing the equivalent of defining a range in Python!
+  // why always rounding down? couldn't I use Math.round()? to not bias possible outcomes.
+  // Math: a group of functions
+  const randomIndex = Math.floor(Math.random() * choiceOptions.length); // Math.floor() rounds down to lower number. Math.ranrom() picks random decimal number between 0 and 1
+  const computerChoice = choiceOptions[randomIndex]; // array of R,P,S. Index result of random number generated
+  return computerChoice;
+}
+
+// structures overarching game
 function playRound() {
   const computerChoice = randomPick();
   const userChoice = getUserChoice();
   declareWinner(userChoice, computerChoice);
 }
 
-function randomPick() {
-  const choiceOptions = Object.keys(acceptableVariants);
-  const randomIndex = Math.floor(Math.random() * choiceOptions.length);
-  const computerChoice = choiceOptions[randomIndex];
-  return computerChoice;
-}
 
 module.exports = {
   asStandardChoice,
